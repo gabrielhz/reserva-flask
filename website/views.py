ba57d9a4a444ada3f09ff98f92c78f14476b00b1
Gabriel Ramos <gabrielramoscardoso@hotmail.com>
2023-06-22 20:26:05 -0300

    register restaurant page and views

--- website/views.py
+++ website/views.py
@@ -14,8 +14,29 @@ def home():
     return render_template("home.html", restaurantes=restaurantes)
 
 
+@views.route('/add-restaurante', methods=['GET', 'POST'])
+def add_restaurante():
+    if request.method == 'POST':
+        nomeRestaurante = request.form.get('nomeRestaurante')
+        enderecoRestaurante = request.form.get('enderecoRestaurante')
+
+        restaurante = Restaurante.query.filter_by(
+            nome_restaurante=nomeRestaurante).first()
+        if restaurante:
+            flash('Restaurante já registrado!', category='error')
+        else:
+            new_restaurante = Restaurante(
+                nome_restaurante=nomeRestaurante, endereco=enderecoRestaurante)
+            db.session.add(new_restaurante)
+            db.session.commit()
+            flash('Restaurante Adicionado!', category='sucess')
+            return redirect(url_for('views.home'))
+
+    return render_template('add_restaurante.html')
+
+
 @views.route('/restaurante/<int:restaurante_id>')
 def restaurante(restaurante_id):
-    patrimonio = Restaurante.query.get(restaurante_id)
+    restaurante = Restaurante.query.get(restaurante_id)
 
-    return render_template("item_info.html", restaurante=restaurante)
+    return render_template("restaurante.html", restaurante=restaurante)
